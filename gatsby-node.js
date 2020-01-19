d5c31f814b0181e2bfa7ce2916403b3804a0b0d9
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-01-19 15:39:39 -0800

    Allow for other md files

--- gatsby-node.js
+++ gatsby-node.js
@@ -35,46 +35,48 @@ exports.createPages = async function createPages({
   data.allMarkdownRemark.edges.forEach(item => {
     const { uid, appStoreId } = item.node.frontmatter
 
-    createPage({
-      path: `/games/${uid}/`,
-      component: path.resolve('src/templates/game.tsx'),
-      context: {
-        uid,
-        appStoreId,
-      },
-    })
+    if (appStoreId) {
+      createPage({
+        path: `/games/${uid}/`,
+        component: path.resolve('src/templates/game.tsx'),
+        context: {
+          uid,
+          appStoreId,
+        },
+      })
+    }
   })
 }
 
 exports.onCreateNode = async ({
   node,
-  actions,
+  actions: { createNode },
   createNodeId,
   createContentDigest,
 }) => {
-  const { createNode } = actions
-
-  const createNodeById = async id => {
+  if (
+    node.internal.type === 'MarkdownRemark' &&
+    node.frontmatter &&
+    !!node.frontmatter.appStoreId
+  ) {
+    const id = node.frontmatter.appStoreId
     const req = await fetch(`https://itunes.apple.com/lookup?id=${id}`)
     const data = await req.json().then(d => d.results[0])
 
-    const nodeMeta = {
-      id: createNodeId(`my-data-${id}`),
-      uid: id,
-      data: JSON.stringify(data),
-      parent: null,
-      children: [],
-      internal: {
-        type: `appStoreData`,
-        mediaType: `text/html`,
-        contentDigest: createContentDigest(data),
+    await createNode({
+      ...data,
+      ...{
+        id: createNodeId(`my-data-${id}`),
+        uid: id,
+        data: JSON.stringify(data),
+        parent: null,
+        children: [],
+        internal: {
+          type: `appStoreData`,
+          mediaType: `text/html`,
+          contentDigest: createContentDigest(data),
+        },
       },
-    }
-
-    await createNode({ ...data, ...nodeMeta })
-  }
-
-  if (node.internal.type === 'MarkdownRemark') {
-    await createNodeById(node.frontmatter.appStoreId)
+    })
   }
 }
