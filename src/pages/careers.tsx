d03001334c7df4b21ed01cf190f744fc0e8aad21
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-01-28 18:47:44 -0800

    embed career data

--- src/pages/careers.tsx
+++ src/pages/careers.tsx
@@ -1,6 +1,42 @@
+/* eslint-disable camelcase */
 import React from 'react'
 import Layout from 'components/Layout'
 import { Wrap, Section } from 'components/UI'
+import { useStaticQuery, graphql } from 'gatsby'
+
+export const query = graphql`
+  {
+    allJazzHrJob(filter: { status: { eq: "Open" } }) {
+      edges {
+        node {
+          title
+          department
+          city
+          state
+          description
+          type
+          board_code
+        }
+      }
+    }
+  }
+`
+
+type Data = {
+  allJazzHrJob: {
+    edges: {
+      node: {
+        title: string
+        department: string
+        city: string
+        state: string
+        description: string
+        type: string
+        board_code: string
+      }
+    }[]
+  }
+}
 
 const Careers = () => {
   return (
@@ -9,9 +45,34 @@ const Careers = () => {
         <Section>
           <h1>Careers</h1>
           <p>Work Here!</p>
+          <List />
         </Section>
       </Wrap>
     </Layout>
+  )
+}
+
+const List = () => {
+  const queryRes: Data = useStaticQuery(query)
+  const data = queryRes.allJazzHrJob.edges.map(item => item.node)
+  return (
+    <>
+      {data.map(({ city, title, board_code: code, state }) => (
+        <>
+          <a
+            href={`https://brainiumstudios.applytojob.com/apply/${code}`}
+            target="blank"
+            rel="noopener noreferrer"
+          >
+            <div>
+              <strong>{title}</strong>
+            </div>
+            {city}, {state}
+          </a>
+          <hr />
+        </>
+      ))}
+    </>
   )
 }
 
