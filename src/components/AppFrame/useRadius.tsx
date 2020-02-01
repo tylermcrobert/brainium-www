60f138f7baf2e0a254f1fb360cb0ce670de13d2d
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-02-01 11:16:11 -0800

    use resize observer for corners

--- src/components/AppFrame/useRadius.tsx
+++ src/components/AppFrame/useRadius.tsx
@@ -1,27 +1,24 @@
 import { useEffect, useRef } from 'react'
+import ResizeObserver from 'resize-observer-polyfill'
 
 const useRadius = (radius: number) => {
   const ref = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    const getCorner = () => {
-      if (ref.current) {
-        // get radius from edge
-        const { width, height } = ref.current.getBoundingClientRect()
-        const smallestEdge = Math.min(width, height)
-        const divRadius = smallestEdge * (radius / 100)
+    const ro = new ResizeObserver(entries => {
+      const {
+        contentRect: { width, height },
+        target,
+      } = entries[0]
+      const smallestEdge = Math.min(width, height)
+      const divRadius = smallestEdge * (radius / 100)
+      const div = target as HTMLDivElement
 
-        // set radius
-        ref.current.style.borderRadius = `${divRadius}px`
-      }
-    }
+      div.style.borderRadius = `${divRadius}px`
+    })
 
-    getCorner()
-
-    window.addEventListener('resize', getCorner)
-
-    return () => {
-      window.removeEventListener('resize', getCorner)
+    if (ref.current) {
+      ro.observe(ref.current)
     }
   }, [])
 
